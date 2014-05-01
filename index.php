6fea26f73915262fb962d3eda2732d48cf175a1f
Jean-francois lefebvre <lefebvre.jf@gmail.com>
2014-05-01 22:06:53 +0200

    -- do not force to display errors

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set("display_errors", 1);
+//error_reporting(E_ALL);
+//ini_set("display_errors", 1);
 
 /** 
   * @desc A PHP script displaying Pinterest pins of a user using WookMark jQuery plugin
